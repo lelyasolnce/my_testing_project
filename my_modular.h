cda9f051c90e3a63123331400888c9e6816de938
lelyasolnce <lelya-solnce.2010@yandex.ru>
2015-08-17 15:08:31 +0400

    Update my_modular.h

--- my_modular.h
+++ my_modular.h
@@ -4,57 +4,60 @@
 namespace Givaro
 {
 	template <class XXX>
-	class Modular<XXX>
+	class Modular
 	{
 	public:
 		typedef XXX Element;
 		typedef Modular<XXX> Self_t;
 		typedef uint64_t Residu_t;
 		// ----- Constantes
-		const Element zero;
-		const Element one;
-		const Element mOne;
-		const Element p;
+		Element zero;
+		Element one;
+		Element mOne;
+		Element _p;
 		// ----- Constructors
 		Modular();
 		Modular(Residu_t p);
 		Modular(const Self_t& F);
+		Self_t& operator= (const Self_t& A);
 	};
 
 	template <>
 	class Modular<double>
 	{
 	public:
-		typedef XXX Element;
-		typedef Modular<XXX> Self_t;
+		typedef double Element;
+		typedef Modular<double> Self_t;
 		typedef uint64_t Residu_t;
 		// ----- Constantes
-		const Element zero;
-		const Element one;
-		const Element mOne;
-		const Element _p;
+		Element zero;
+		Element one;
+		Element mOne;
+		Element _p;
 		// ----- Constructors
 		Modular() : zero(0.0), one(1.0), mOne(-1.0), _p(0.0) {}
-		Modular(const XXX& p) : zero(0.0), one(1.0), mOne((Element)p - 1.0), _p((double)p) {}
+		Modular(const Residu_t& p) : zero(0.0), one(1.0), mOne((Element)p - 1.0), _p((double)p) {}
 		Modular(const Self_t& F) : zero(F.zero), one(F.one), mOne(F.mOne), _p(F._p) {}
+		Self_t& operator= (const Self_t& F) { zero = F.zero; one = F.one; mOne = F.mOne; _p = F._p;}
 	};
 
 	template<>
 	class Modular<int64_t> 
 	{
 	public:
-		typedef XXX Element;
-		typedef Modular<XXX> Self_t;
+		typedef int64_t Element;
+		typedef Modular<int64_t> Self_t;
 		typedef uint64_t Residu_t;
 		// ----- Constantes
-		const Element zero;
-		const Element one;
-		const Element mOne;
-		const Element p;
+		Element zero;
+		Element one;
+		Element mOne;
+		Element _p;
 		// ----- Constructors
 		Modular() : zero(0), one(1), mOne(-1), _p(0) {}
 		Modular(Residu_t p) : zero(0), one(1), mOne((Element)p-1), _p(p) {}
 		Modular(const Self_t& F) : zero(F.zero), one(F.one), mOne(F.mOne), _p(F._p) {}
+		Self_t& operator= (const Self_t& F) { zero = F.zero; one = F.one; mOne = F.mOne; _p = F._p;}
 	};
 }
 
