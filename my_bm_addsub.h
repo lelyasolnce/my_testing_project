2088b079bf8c163af5162317acd5ad5c97b389d7
lelyasolnce <lelya-solnce.2010@yandex.ru>
2015-08-17 15:54:20 +0400

    Update my_bm_addsub.h

--- my_bm_addsub.h
+++ my_bm_addsub.h
@@ -10,11 +10,13 @@ namespace LinBox
 		Matrix1& operator() (Field& F, Matrix1& C, Matrix2& A, Matrix3& B)
 		{
 			typename Matrix2::Element el2; typename Matrix3::Element el3;
+			typename Field::Element p; p = F._p;
 			for (size_t i = 0; i < C.rowdim(); i++)
 			{
 				for (size_t j = 0; j < C.coldim(); j++)
 				{
-					el2 = A.getEntry(i,j); el3 = B.getEntry(i,j); el2 = el2+el3;
+					el2 = A.getEntry(i,j); el3 = B.getEntry(i,j);
+					el2 = el2+el3; el2 = el2 % p;
 					C.setEntry(i, j, el2);
 				}
 			}
@@ -29,11 +31,13 @@ namespace LinBox
 		Matrix1& operator() (Field& F, Matrix1& C, Matrix2& A, Matrix3& B)
 		{
 			typename Matrix2::Element el2; typename Matrix3::Element el3;
+			typename Field::Element p; p = F._p;
 			for (size_t i = 0; i < C.rowdim(); i++)
 			{
 				for (size_t j = 0; j < C.coldim(); j++)
 				{
-					el2 = A.getEntry(i,j); el3 = B.getEntry(i,j); el2 = el2-el3;
+					el2 = A.getEntry(i,j); el3 = B.getEntry(i,j);
+					el2 = p + el2 - el3; el2 = el2 % p;
 					C.setEntry(i, j, el2);
 				}
 			}
@@ -48,11 +52,13 @@ namespace LinBox
 		Matrix1& operator() (Field& F, Matrix1& C, Matrix2& A)
 		{
 			typename Matrix2::Element el2; typename Matrix1::Element el3;
+			typename Field::Element p; p = F._p;
 			for (size_t i = 0; i < C.rowdim(); i++)
 			{
 				for (size_t j = 0; j < C.coldim(); j++)
 				{
-					el2 = A.getEntry(i,j); el3 = C.getEntry(i,j); el2 = el2+el3;
+					el2 = A.getEntry(i,j); el3 = C.getEntry(i,j);
+					el2 = el2 + el3; el2 = el2 % p;
 					C.setEntry(i, j, el2);
 				}
 			}
@@ -67,12 +73,41 @@ namespace LinBox
 		Matrix1& operator() (Field& F, Matrix1& C, Matrix2& A)
 		{
 			typename Matrix2::Element el2; typename Matrix1::Element el3;
+			typename Field::Element p; p = F._p;
 			for (size_t i = 0; i < C.rowdim(); i++)
 			{
 				for (size_t j = 0; j < C.coldim(); j++)
 				{
-					el2 = A.getEntry(i,j); el3 = C.getEntry(i,j); el2 = el3 - el2;
+					el2 = A.getEntry(i,j); el3 = C.getEntry(i,j);
+					el2 = p + el3 - el2; el2 = el2 % p;
 					C.setEntry(i, j, el2);
+				}
+			}
+			return C;
+		}
+	};
+
+	template<class Field, class Matrix1, class Matrix2, class Matrix3>
+	class BlasMatrixDomainMul
+	{
+	public:
+		Matrix1& operator() (Field& F, Matrix1& C, Matrix2& A, Matrix3& B)
+		{
+			typename Matrix1::Element el1;
+			typename Matrix2::Element el2; typename Matrix3::Element el3;
+			typename Field::Element p; p = F._p;
+			for (size_t i = 0; i < C.rowdim(); i++)
+			{
+				for (size_t j = 0; j < C.coldim(); j++)
+				{
+					el1 = 0;
+					for (size_t k = 0; k < B.rowdim(); k++)
+					{
+						el2 = A.getEntry(i,k); el3 = B.getEntry(k,j);
+						el1 += (el2 * el3);
+					}					
+					el1 = el1 % p;
+					C.setEntry(i, j, el1);
 				}
 			}
 			return C;
