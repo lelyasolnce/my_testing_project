324b82c93294664b6c5e0cff3c2f67751439a65a
lelyasolnce <lelya-solnce.2010@yandex.ru>
2015-08-17 15:31:43 +0400

    Update myBM.h

--- myBM.h
+++ myBM.h
@@ -23,7 +23,7 @@ namespace LinBox
 		typedef typename Field::Element               Element;    //!< Element type
 		typedef _Storage                                  Rep;    //!< Actually a <code>std::vector<Element></code> (or alike.)
 		typedef BlasMatrix<Field,Rep>                  Self_t;    //!< Self typeype
-		typedef const BlasMatrix<Field,Rep>       constSelf_t;    //!< Self typeype
+		typedef  BlasMatrix<Field,Rep>       constSelf_t;    //!< Self typeype
 
                 //typedef BlasSubmatrix<Self_t>           subMatrixType;    //!< Submatrix type
 		//typedef BlasSubmatrix<constSelf_t> constSubMatrixType;    //!< Submatrix type
@@ -36,7 +36,11 @@ namespace LinBox
 		size_t _col;
 		Rep	_rep;
 	public:
-	       	const Field		    * _field; //! @bug why public ?
+		typedef Element* Element_ptr;
+		Element_ptr getPointer() {return  &_rep[0];}
+		Element_ptr getWritePointer() {return  &_rep[0];}
+	       	Field		    * _field; //! @bug why public ?
+		Field& field() {return *_field;}
 		//MatrixDomain<Field>    _MD; //! @bug why public ?
 		//VectorDomain<Field>    _VD;
 
@@ -49,14 +53,14 @@ namespace LinBox
 
 
 		/*! Allocates a new \f$ 0 \times 0\f$ matrix (shaped and ready).*/
-		BlasMatrix (const _Field &F)
+		BlasMatrix ( _Field &F)
 		{
 			_row = 0; _col = 0; _field = &F;
 		}
 		
 
 		/*! Allocates a new \f$ m \times n\f$ zero matrix (shaped and ready).		 */
-		BlasMatrix (const _Field &F, const size_t & m, const size_t &n)
+		BlasMatrix ( _Field &F,  size_t & m,  size_t &n)
 		{
 			//_row = m; _col = n; _rep(_row*_col, F.zero); _filed = &F;
 			_row = m; _col = n;
@@ -70,15 +74,15 @@ namespace LinBox
 		// BlasMatrix () ;
 
 		/// (Re)allocates a new \f$ m \times n\f$ zero matrix (shaped and ready).
-		void init(const _Field & F, const size_t & r = 0, const size_t & c = 0);
+		void init( _Field & F,  size_t & r = 0,  size_t & c = 0);
 
 		//! Generic copy constructor from either a blackbox or a matrix container.
 		template <class Matrix>
-		BlasMatrix (const Matrix &A) ;
+		BlasMatrix ( Matrix &A) ;
 
 		//! Create a BlasMatrix from a vector of elements
-		BlasMatrix (const _Field &F, const std::vector<Element>& v,
-			    const size_t &m , const size_t &n) ;
+		BlasMatrix ( _Field &F,  std::vector<Element>& v,
+			     size_t &m ,  size_t &n) ;
 		*/
 
 		/// Destructor.
@@ -88,17 +92,17 @@ namespace LinBox
 		//  DIMENSIONS  //
 		//////////////////
 
-		size_t rowdim() const {return _row;}
-		size_t coldim() const {return _col;}
-		size_t getStride() const {return _col;}
-		size_t stride() const { return getStride() ;}
+		size_t rowdim()  {return _row;}
+		size_t coldim()  {return _col;}
+		size_t getStride()  {return _col;}
+		size_t stride()  { return getStride() ;}
 
 		//////////////////
 		//   ELEMENTS   //
 		//////////////////
 
 	
-		void setEntry (size_t i, size_t j, const Element &a_ij) {_rep[i * _col + j] = a_ij;}
+		void setEntry (size_t i, size_t j,  Element &a_ij) {_rep[i * _col + j] = a_ij;}
 		Element &refEntry (size_t i, size_t j) {return _rep[i * _col + j];}
 		Element &getEntry (size_t i, size_t j) {return _rep[i * _col + j];}
 		Element &getEntry (Element &x, size_t i, size_t j) {return _rep[i * _col + j];}
@@ -123,7 +127,7 @@ namespace LinBox
 		 * @param[in] tM
 		 * @return the transposed matrix of this.
 		 */
-		//Self_t transpose(Self_t & tM) const ;
+		//Self_t transpose(Self_t & tM)  ;
 		//void transpose() ; //inplace
 
 	/*
@@ -134,7 +138,7 @@ namespace LinBox
 		}
 
 		template<class Rand>
-		void random(const Rand&)
+		void random( Rand&)
 		{
 			return random();
 		}
