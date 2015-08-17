3bc5f854902bed42e153ea8afcb1f9464f1add12
lelyasolnce <lelya-solnce.2010@yandex.ru>
2015-08-17 16:47:21 +0400

    Update tests_SPM_io.cpp

--- tests_SPM_io.cpp
+++ tests_SPM_io.cpp
@@ -16,10 +16,17 @@ int main()
 	GField _gf(7, 3); int m = 2, n = 5;
 	std::filebuf fb1; fb1.open("file1.txt", std::ios::in); std::istream file1(&fb1);
 	std::filebuf fb2; fb2.open("file2.txt", std::ios::in); std::istream file2(&fb2);
+	std::filebuf fb3; fb3.open("file3.txt", std::ios::out); std::ostream file3(&fb3);
 	SPM spm1(_gf, m, n); SPM spm2(_gf, m, n); spm1.read(file1); spm2.read(file2);
-	SPM spm3(_gf, m, n);
+	SPM spm3(_gf, m, n); 
+	
+	
 	LinBox::SlicedPolynomialMatrixAdd<GField, SPM, SPM, SPM>()(_gf, spm3, spm1, spm2);
+	file3<<"\nC = A + B\n"; spm3.write(file3);
 	LinBox::SlicedPolynomialMatrixSub<GField, SPM, SPM, SPM>()(_gf, spm3, spm1, spm2);
+	file3<<"\nC = A - B\n"; spm3.write(file3);
 	LinBox::SlicedPolynomialMatrixAddin<GField, SPM, SPM>()(_gf, spm3, spm1);
+	file3<<"\nC += A\n"; spm3.write(file3);
 	LinBox::SlicedPolynomialMatrixSubin<GField, SPM, SPM>()(_gf, spm3, spm1);
+	file3<<"\nC -= A\n"; spm3.write(file3);
 }
